23f3698351b2ced0611755783774cc1ca8a3d10a
Glenn <glenn@neondigital.co.uk>
2017-01-26 12:22:58 +0000

    Passing binPath to PdfFactory to make Windows happy

--- src/Ignited/Pdf/PdfServiceProvider.php
+++ src/Ignited/Pdf/PdfServiceProvider.php
@@ -1,56 +1,53 @@
-<?php namespace Ignited\Pdf;
+<?php
 
-use Illuminate\Support\ServiceProvider;
+namespace Ignited\Pdf;
 
 use Ignited\Pdf\PdfFactory;
+use Illuminate\Support\ServiceProvider;
 
-class PdfServiceProvider extends ServiceProvider {
+class PdfServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['pdf'] = $this->app->share(function ($app) {
+            $config = $app['config']['laravel-pdf'] ?: $app['config']['laravel-pdf::config'];
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['pdf'] = $this->app->share(function($app)
-        {
-        	$config = $app['config']['laravel-pdf'] ?: $app['config']['laravel-pdf::config'];
+            if (!$config || !$config['binPath']) {
+                throw new \RunTimeException('Bin path for wkhtmltopdf is not configured.');
+            }
 
-        	if(!$config || !$config['bin'])
-        	{
-        		throw new \RunTimeException('Bin path for wkhtmltopdf is not configured.');
-        	}
+            if (!file_exists($config['binPath'])) {
+                throw new \RunTimeException('Cannot find bin for wkhtmltopdf - have you included it in composer?');
+            }
 
-        	if(!file_exists($config['bin']))
-        	{
-        		throw new \RunTimeException('Cannot find bin for wkhtmltopdf - have you included it in composer?');
-        	}
-
-        	return new PdfFactory($config);
+            return new PdfFactory($config);
         });
-	}
+    }
 
-	public function boot()
-	{
-		$this->package('ignited/laravel-pdf');
-	}
+    public function boot()
+    {
+        $this->package('ignited/laravel-pdf');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
